728603cd3caa766efe2dacab2eab2c2f5861a30d
Manhal Munir <manhal_muner88@hotmail.com>
2017-11-17 20:12:51 +0100

    Create IkkeFerdige.java

---
+++ Sem2-Oblig3-SudokuLoser-Program/IkkeFerdige.java
@@ -0,0 +1,28 @@
+
+
+public class IkkeFerdige extends Rute  {
+	IkkeFerdige(int nummer,Kolonne kolonne, Rad rad, Boks boks, Brett brett){
+		super(nummer,kolonne, rad, boks, brett);
+	}
+	/*
+	 * (non-Javadoc)
+	 * @see Oblig.Rute#settTallMegOgResten()
+	 * 
+	 * metoden sjekker om verdien til et tall i rutene om den finnes i rad,kolonne eller boks. hvis ikke s� setter rute verdi til tallet
+	 */
+	public void settTallMegOgResten() {
+		for (int s  = 1; s <= rad.dim; s++) {
+			if (rad.sjekkTall(s) == false && kolonne.sjekkTall(s) == false && boks.sjekkTall(s) == false) {
+				settVerdi(s);
+				if( neste == null) {
+					antallLosninger++;
+					brett.leggTilLosningen(antallLosninger);
+				}
+				else {
+					neste.settTallMegOgResten();
+				}
+			}
+			settVerdi(0);
+		}
+	}
+}
